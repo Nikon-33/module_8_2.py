f5b34e3296e46a2f7ac49d78c4a984c04e9c9602
Nikon-33 <89101717800@mail.ru>
2024-07-31 15:41:55 +0300

    Add files via upload

--- module_8_2.py
+++ module_8_2.py
@@ -7,12 +7,13 @@ def personal_sum(numbers):
         except TypeError as exc:
             print(f'Некорректный тип данных для подсчёта суммы - {i}')
             incorrect_data += 1
-    return result
+    return result, incorrect_data
 
 
 def calculate_average(numbers):
     try:
         num = personal_sum(numbers)
+        num_ = num[0]
     except TypeError:
         print(f'В numbers записан некорректный тип данных')
         return None
@@ -21,7 +22,7 @@ def calculate_average(numbers):
         if isinstance(i, (int, float)):
             del_ += 1
     try:
-        avg = num / del_
+        avg = num_ / del_
     except ZeroDivisionError:
         return 0
     return avg
